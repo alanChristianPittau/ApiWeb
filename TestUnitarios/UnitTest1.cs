574f28f9eb441a70aff5768c75a212ac609db43c
Alan Pitauu <pittaualan@gmail.com>
2022-11-14 03:39:52 -0300

    se agrega proyectos de test unitarios

---
+++ TestUnitarios/UnitTest1.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Services.Taller;
+
+namespace TestUnitarios
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestSumar()
+        {
+            //Dados
+      
+
+            //Cuando
+            //int calculado = Program.Addition(num1, num2);
+            //Assert.AreEqual(esperado, calculado);
+
+            TallerServices tallerServices = new TallerServices();
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            //Assert.Pass();
+        }
+    }
+}
